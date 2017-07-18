75949196205a1080c703cfc2808a1fa2e6d4517c
PlanetTeamSpeak <PlanetTeamSpeakk@users.noreply.github.com>
2017-07-18 19:07:32 +0200

    Updated to 1.10

--- src/main/java/com/ptsmods/morecommands/MoreCommands.java
+++ src/main/java/com/ptsmods/morecommands/MoreCommands.java
@@ -2,28 +2,34 @@ package com.ptsmods.morecommands;
 
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 @Mod(modid=Reference.MOD_ID, name=Reference.MOD_NAME, version=Reference.VERSION)
 public class MoreCommands {
 	
 	@EventHandler
 	public void serverLoad(FMLServerStartingEvent event) {
-		Initialize.register(event);
+		Initialize.registerCommands(event);
 		Reference.setServerStartingEvent(event);
 	}
 	
-//	@EventHandler
-//	public void preInit(FMLPreInitializationEvent event) {
-	
-//	}
+	@EventHandler
+	@SideOnly(Side.CLIENT)
+	public void preInit(FMLPreInitializationEvent event) {
+        try {
+        	Initialize.registerClientCommands();
+        } catch (NoClassDefFoundError e) {
+        	System.out.println("An error occured while loading the client sided MoreCommands commands, if the mod is installed on a server you can ignore this error, if not please contact PlanetTeamSpeak.");
+        	System.out.println(e.getStackTrace());
+        }
+	}
 	
 //	@EventHandler
 //	public void init(FMLInitializationEvent event) {
-		
+
 //	}
 	
 //	@EventHandler
