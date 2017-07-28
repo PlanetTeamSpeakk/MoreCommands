9380bba7f7775c9dbf5ea23bb769433397144723
PlanetTeamSpeak <PlanetTeamSpeakk@users.noreply.github.com>
2017-07-28 13:52:19 +0200

    Updated to 1.16

--- src/main/java/com/ptsmods/morecommands/Initialize.java
+++ src/main/java/com/ptsmods/morecommands/Initialize.java
@@ -11,7 +11,9 @@ import com.ptsmods.morecommands.commands.consoleCommand.CommandconsoleCommand;
 import com.ptsmods.morecommands.commands.day.Commandday;
 import com.ptsmods.morecommands.commands.descend.Commanddescend;
 import com.ptsmods.morecommands.commands.easy.Commandeasy;
+import com.ptsmods.morecommands.commands.enchant.Commandenchant;
 import com.ptsmods.morecommands.commands.explode.Commandexplode;
+import com.ptsmods.morecommands.commands.fixTime.CommandfixTime;
 import com.ptsmods.morecommands.commands.fly.Commandfly;
 import com.ptsmods.morecommands.commands.fullbright.Commandfullbright;
 import com.ptsmods.morecommands.commands.gm.Commandgm;
@@ -31,6 +33,7 @@ import com.ptsmods.morecommands.commands.noHunger.CommandnoHunger;
 import com.ptsmods.morecommands.commands.noRain.CommandnoRain;
 import com.ptsmods.morecommands.commands.normal.Commandnormal;
 import com.ptsmods.morecommands.commands.opTool.CommandopTool;
+import com.ptsmods.morecommands.commands.pastNames.CommandpastNames;
 import com.ptsmods.morecommands.commands.peaceful.Commandpeaceful;
 import com.ptsmods.morecommands.commands.ping.Commandping;
 import com.ptsmods.morecommands.commands.powerTool.CommandpowerTool;
@@ -41,6 +44,7 @@ import com.ptsmods.morecommands.commands.repair.Commandrepair;
 import com.ptsmods.morecommands.commands.resetNBT.CommandresetNBT;
 import com.ptsmods.morecommands.commands.runcmd.Commandruncmd;
 import com.ptsmods.morecommands.commands.save.Commandsave;
+import com.ptsmods.morecommands.commands.serverStatus.CommandserverStatus;
 import com.ptsmods.morecommands.commands.setBuildLimit.CommandsetBuildLimit;
 import com.ptsmods.morecommands.commands.setFOV.CommandsetFOV;
 import com.ptsmods.morecommands.commands.setRenderDistance.CommandsetRenderDistance;
@@ -59,8 +63,9 @@ import com.ptsmods.morecommands.commands.tpaccept.Commandtpaccept;
 import com.ptsmods.morecommands.commands.tpdeny.Commandtpdeny;
 import com.ptsmods.morecommands.commands.vanish.Commandvanish;
 import com.ptsmods.morecommands.commands.wild.Commandwild;
-import com.ptsmods.morecommands.miscellaneous.EventHandler;
+import com.ptsmods.morecommands.miscellaneous.ClientEventHandler;
 import com.ptsmods.morecommands.miscellaneous.Reference;
+import com.ptsmods.morecommands.miscellaneous.ServerEventHandler;
 
 import net.minecraft.block.Block;
 import net.minecraft.command.ICommand;
@@ -76,12 +81,13 @@ public class Initialize {
 	public static void registerCommands(FMLServerStartingEvent event) {
 		System.out.println("Registering MoreCommands commands.");
 		
-		ICommand[] commands = {new Commandday(), new Commandnight(), new Commandexplode(), new CommandkillAll(), new CommandclearInv(), new Commandspeed(), new CommandclearEffects(), new Commandheal(), new Commandgma(),
-				new Commandgms(), new Commandgmc(), new Commandgmsp(), new Commanditem(), new Commandgm(), new Commandhard(), new Commandnormal(), new Commandeasy(), new Commandpeaceful(), new CommandshowTime(),
-				new Commandcmdblock(), new Commandbarrier(), new CommandnoRain(), new Commandkys(), new CommandnoHunger(), new Commandgod(), new Commandsudo(), new CommandsetBuildLimit(), new Commandsave(), new Commandtop(),
-				new CommandconsoleCommand(), new CommandreloadMoreCommands(), new Commandfly(), new Commandtpa(), new Commandtpaccept(), new Commandtpdeny(), new Commandwild(), new Commandspawn(), new Commandbreak(),
-				new Commandsmite(), new Commandthrough(), new Commandping(), new Commandrepair(), new Commandrename(), new Commandhat(), new CommandpowerTool(), new CommandresetNBT(), new CommandshowNBT(),
-				new Commandvanish(), new Commanddescend(), new Commandascend(), new CommandopTool(), new CommandsuperPickaxe()};
+		ICommand[] commands = {new Commandday(), new Commandnight(), new Commandexplode(), new CommandkillAll(), new CommandclearInv(), new Commandspeed(), new CommandclearEffects(), new Commandheal(), 
+				new Commandgma(), new Commandgms(), new Commandgmc(), new Commandgmsp(), new Commanditem(), new Commandgm(), new Commandhard(), new Commandnormal(), new Commandeasy(), new Commandpeaceful(), 
+				new CommandshowTime(), new Commandcmdblock(), new Commandbarrier(), new CommandnoRain(), new Commandkys(), new CommandnoHunger(), new Commandgod(), new Commandsudo(), new CommandsetBuildLimit(), 
+				new Commandsave(), new Commandtop(), new CommandconsoleCommand(), new CommandreloadMoreCommands(), new Commandfly(), new Commandtpa(), new Commandtpaccept(), new Commandtpdeny(), new Commandwild(), 
+				new Commandspawn(), new Commandbreak(), new Commandsmite(), new Commandthrough(), new Commandping(), new Commandrepair(), new Commandrename(), new Commandhat(), new CommandpowerTool(), 
+				new CommandresetNBT(), new CommandshowNBT(), new Commandvanish(), new Commanddescend(), new Commandascend(), new CommandopTool(), new CommandsuperPickaxe(), new Commandenchant(),
+				new CommandfixTime()};
 		
 		Integer counter = 0;
 		Integer failed = 0;
@@ -107,6 +113,8 @@ public class Initialize {
 			counter += 1;
 		}
 		
+		MinecraftForge.EVENT_BUS.register(new ServerEventHandler());
+		
 		System.out.println("Successfully set up the block blacklist and added " + counter.toString() + " blocks.");
 		System.out.println("Setting up the MoreCommands block whitelist.");
 		
@@ -122,13 +130,13 @@ public class Initialize {
 		}
 		
 		System.out.println("Successfully set up the block whitelist and added " + counter.toString() + " blocks.");
-				
 	}
 	
 	@SideOnly(Side.CLIENT)
 	public static void registerClientCommands() {
 		System.out.println("Registering MoreCommands client sided commands.");
-		ICommand[] commands = {new CommandspawnClientEntity(), new Commandfullbright(), new Commandptime(), new Commandruncmd(), new CommandsetFOV(), new CommandsetRenderDistance(), new Commandcalc()};
+		ICommand[] commands = {new CommandspawnClientEntity(), new Commandfullbright(), new Commandptime(), new Commandruncmd(), new CommandsetFOV(), new CommandsetRenderDistance(), new Commandcalc(),
+				new CommandserverStatus(), new CommandpastNames()};
 
 		Integer counter = 0;
 		Integer fails = 0;
@@ -142,7 +150,7 @@ public class Initialize {
 		
 		System.out.println("Successfully registered " + counter.toString() + " client sided commands, with " + fails.toString() + " fails.");
 		
-		MinecraftForge.EVENT_BUS.register(new EventHandler());
+		MinecraftForge.EVENT_BUS.register(new ClientEventHandler());
 	}
 	
 }
