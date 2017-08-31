f2e58c76a5fc42df656883d8d850412faaec5bdf
PlanetTeamSpeak <PlanetTeamSpeakk@users.noreply.github.com>
2017-08-31 20:57:38 +0200

    Updated to 1.27

--- src/main/java/com/ptsmods/morecommands/MoreCommands.java
+++ src/main/java/com/ptsmods/morecommands/MoreCommands.java
@@ -1,9 +1,6 @@
 package com.ptsmods.morecommands;
 
-import java.io.File;
 import java.io.IOException;
-
-import org.lwjgl.opengl.Display;
 
 import com.ptsmods.morecommands.miscellaneous.ClientEventHandler;
 import com.ptsmods.morecommands.miscellaneous.CommandType;
@@ -21,37 +18,16 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 
 @Mod(modid=Reference.MOD_ID, name=Reference.MOD_NAME, version=Reference.VERSION, acceptedMinecraftVersions=Reference.MC_VERSIONS, updateJSON=Reference.UPDATE_URL)
 public class MoreCommands {
-	
+
+	private static boolean initialized = false;
+
 	public MoreCommands() {
-		try {
-			Reference.setDisplayTitle(Display.getTitle() + " with MinecraftForge");
-		} catch (NoClassDefFoundError e) {}
-		Reference.downloadDependency("http://central.maven.org/maven2/org/javassist/javassist/3.22.0-CR2/javassist-3.22.0-CR2.jar", "javassist.jar");
-		Reference.downloadDependency("http://central.maven.org/maven2/org/reflections/reflections/0.9.11/reflections-0.9.11.jar", "reflections.jar");
-		Reference.downloadDependency("http://central.maven.org/maven2/org/yaml/snakeyaml/1.18/snakeyaml-1.18.jar", "snakeyaml.jar"); 
-		Reference.setupBiomeList();
-		if (!new File("config/MoreCommands/").isDirectory()) new File("config/MoreCommands/").mkdirs();
-		if (!new File("config/MoreCommands/homes.yaml").exists())
-			try {
-				new File("config/MoreCommands/homes.yaml").createNewFile();
-			} catch (IOException e1) {
-				e1.printStackTrace();
-			}
-		if (!new File("config/MoreCommands/warps.yaml").exists())
-			try {
-				new File("config/MoreCommands/warps.yaml").createNewFile();
-			} catch (IOException e1) {
-				e1.printStackTrace();
-			}
-		try {
-			Reference.loadWarpsFile();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		initialize();
 	}
-	
+
 	@EventHandler
 	public void serverLoad(FMLServerStartingEvent event) {
+		initialize();
 		try {
 			Reference.resetCommandRegistry(CommandType.SERVER); // for when you relog and a new command has been added, only needed for development.
 			Reference.resetCommandRegistry(CommandType.CLIENT);
@@ -59,32 +35,29 @@ public class MoreCommands {
 		} catch (IncorrectCommandType e) {
 			e.printStackTrace();
 		}
-		if (Reference.shouldRegisterCommands) Initialize.registerCommands(event);
+		Initialize.registerCommands(event);
 		Initialize.setupBlockLists();
 		try {
 			Reference.registerEventHandler(CommandType.SERVER, new ServerEventHandler());
 		} catch (IncorrectCommandType e) {}
 		Reference.setServerStartingEvent(event);
 	}
-	
+
 	@EventHandler
 	@SideOnly(Side.CLIENT)
 	public void postInit(FMLPostInitializationEvent event) {
-		Initialize.setupCommandRegistry();
-    	if (Reference.shouldRegisterCommands) Initialize.registerClientCommands();
+		Initialize.registerClientCommands();
 	}
-	
+
 	@EventHandler
 	@SideOnly(Side.CLIENT)
 	public void preInit(FMLPreInitializationEvent event) {
-		if (!new File("config/MoreCommands/infoOverlay.txt").exists())
-			try {
-				new File("config/MoreCommands/infoOverlay.txt").createNewFile();
-			} catch (IOException e1) {
-				e1.printStackTrace();
-			}
+		initialize();
+		Reference.createFileIfNotExisting("config/MoreCommands/infoOverlay.txt");
+		Reference.createFileIfNotExisting("config/MoreCommands/aliases.yaml");
 		try {
 			Reference.loadInfoOverlayConfig();
+			Reference.loadAliases();
 		} catch (IOException e1) {
 			e1.printStackTrace();
 		}
@@ -93,5 +66,31 @@ public class MoreCommands {
 			Reference.registerEventHandler(CommandType.CLIENT, new ClientEventHandler());
 		} catch (IncorrectCommandType e) {}
 	}
-	
+
+	private static void initialize() {
+		if (!initialized) {
+			//			try {
+			//				Downloader.downloadDependency("http://central.maven.org/maven2/org/javassist/javassist/3.22.0-CR2/javassist-3.22.0-CR2.jar", "javassist.jar");
+			//				if (new File("libs/reflections.jar").exists()) new File("libs/reflections.jar").delete(); // getting ready to downgrade to 0.9.10 since 0.9.11 gives an error.
+			//				Downloader.downloadDependency("http://central.maven.org/maven2/org/reflections/reflections/0.9.10/reflections-0.9.10.jar", "reflections.jar");
+			//				Downloader.downloadDependency("http://central.maven.org/maven2/org/yaml/snakeyaml/1.18/snakeyaml-1.18.jar", "snakeyaml.jar");
+			//				Downloader.downloadDependency("http://central.maven.org/maven2/com/google/guava/guava/23.0/guava-23.0.jar", "guava.jar");
+			//			} catch (NoSuchMethodException | SecurityException | IllegalAccessException | IllegalArgumentException
+			//					| InvocationTargetException | IOException e2) {
+			//				e2.printStackTrace();
+			//			} // using shadowJar instead now.
+			//			try {
+			//				Downloader.addJarToClasspath("libs/javassist.jar");
+			//				Downloader.addJarToClasspath("libs/reflections.jar");
+			//				Downloader.addJarToClasspath("libs/snakeyaml.jar");
+			//				Downloader.addJarToClasspath("libs/guava.jar");
+			//			} catch (NoSuchMethodException | SecurityException | IllegalAccessException | IllegalArgumentException
+			//					| InvocationTargetException | MalformedURLException e) {
+			//				e.printStackTrace();
+			//			} just let forge handle it.
+			Reference.initialize();
+			initialized = true;
+		}
+	}
+
 }
