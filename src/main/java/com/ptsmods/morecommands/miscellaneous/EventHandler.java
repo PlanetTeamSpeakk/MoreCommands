3e8434bcd7b5a81c1c4a3c0ee28ba732164bb6a3
PlanetTeamSpeak <PlanetTeamSpeakk@users.noreply.github.com>
2019-05-30 16:12:02 +0200

    Deleted source
    Master branch does not need these files, look at the versioned branches for source code.

--- src/main/java/com/ptsmods/morecommands/miscellaneous/EventHandler.java
+++
@@ -1,5 +0,0 @@
-package com.ptsmods.morecommands.miscellaneous;
-
-public class EventHandler {
-	// just for so the Reference.registerEventHandler() method doesn't get passed a non-eventhandler object.
-}
