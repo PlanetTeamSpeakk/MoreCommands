8096da428bfe0a47c365be3c53cfa6ee821929d7
PlanetTeamSpeak <PlanetTeamSpeakk@users.noreply.github.com>
2022-06-06 17:45:08 +0200

    Check gamerule before translating formattings

--- common/src/main/java/com/ptsmods/morecommands/commands/server/elevated/RenameCommand.java
+++ common/src/main/java/com/ptsmods/morecommands/commands/server/elevated/RenameCommand.java
@@ -4,6 +4,7 @@ import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import com.ptsmods.morecommands.MoreCommands;
 import com.ptsmods.morecommands.miscellaneous.Command;
+import com.ptsmods.morecommands.miscellaneous.MoreGameRules;
 import net.minecraft.item.ItemStack;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.util.Formatting;
@@ -17,7 +18,10 @@ public class RenameCommand extends Command {
                             ItemStack stack = ctx.getSource().getPlayerOrThrow().getMainHandStack();
                             if (stack.isEmpty()) sendError(ctx, "You are not holding an item.");
                             else {
-                                String name = MoreCommands.translateFormattings(ctx.getArgument("name", String.class));
+                                String nameRaw = ctx.getArgument("name", String.class);
+                                String name = MoreGameRules.get().checkBooleanWithPerm(ctx.getSource().getWorld().getGameRules(),
+                                        MoreGameRules.get().doItemColoursRule(), ctx.getSource().getEntity()) ?
+                                        MoreCommands.translateFormattings(nameRaw) : nameRaw;
                                 stack.setCustomName(literalText(name).build());
                                 sendMsg(ctx, "The item has been renamed to " + name + Formatting.RESET + ".");
                                 return 1;
