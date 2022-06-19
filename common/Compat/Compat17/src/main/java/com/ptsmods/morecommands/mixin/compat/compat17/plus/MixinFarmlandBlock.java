8344814053d23023072379ec508dcca7897ef193
PlanetTeamSpeak <PlanetTeamSpeakk@users.noreply.github.com>
2022-06-19 19:37:13 +0200

    Fix farmland blocks being trampled by any entity

--- common/Compat/Compat17/src/main/java/com/ptsmods/morecommands/mixin/compat/compat17/plus/MixinFarmlandBlock.java
+++ common/Compat/Compat17/src/main/java/com/ptsmods/morecommands/mixin/compat/compat17/plus/MixinFarmlandBlock.java
@@ -3,19 +3,17 @@ package com.ptsmods.morecommands.mixin.compat.compat17.plus;
 import com.ptsmods.morecommands.api.IMoreGameRules;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.FarmlandBlock;
-import net.minecraft.entity.Entity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(FarmlandBlock.class)
 public class MixinFarmlandBlock {
 
-    @Inject(at = @At("HEAD"), method = "onLandedUpon")
-    private void onLandedUpon_setToDirt(World world, BlockState state, BlockPos pos, Entity entity, float fallDistance, CallbackInfo cbi) {
+    @Redirect(at = @At(value = "INVOKE", target = "Lnet/minecraft/block/FarmlandBlock;setToDirt(Lnet/minecraft/block/BlockState;Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;)V"), method = "onLandedUpon")
+    private void onLandedUpon_setToDirt(BlockState state, World world, BlockPos pos) {
         if (world.getGameRules().getBoolean(IMoreGameRules.get().doFarmlandTrampleRule())) FarmlandBlock.setToDirt(state, world, pos);
     }
 }
