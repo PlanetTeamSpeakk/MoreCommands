d88bcad6577b1ac24b4b651a98fb91678e8f2ca0
PlanetTeamSpeak <PlanetTeamSpeakk@users.noreply.github.com>
2022-09-11 16:42:38 +0200

    Don't use database for homes

--- common/src/main/java/com/ptsmods/morecommands/commands/server/unelevated/HomeCommand.java
+++ common/src/main/java/com/ptsmods/morecommands/commands/server/unelevated/HomeCommand.java
@@ -5,15 +5,10 @@ import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import com.ptsmods.morecommands.MoreCommands;
+import com.ptsmods.morecommands.api.MoreCommandsArch;
 import com.ptsmods.morecommands.miscellaneous.Command;
 import com.ptsmods.morecommands.miscellaneous.MoreGameRules;
 import com.ptsmods.morecommands.mixin.common.accessor.MixinEntityAccessor;
-import com.ptsmods.mysqlw.query.QueryCondition;
-import com.ptsmods.mysqlw.query.builder.InsertBuilder;
-import com.ptsmods.mysqlw.table.ColumnType;
-import com.ptsmods.mysqlw.table.TableIndex;
-import com.ptsmods.mysqlw.table.TablePreset;
-import dev.architectury.event.events.common.PlayerEvent;
 import net.minecraft.ChatFormatting;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.core.Registry;
@@ -22,44 +17,21 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
+
+import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.*;
 import java.util.function.BinaryOperator;
 import java.util.stream.Collector;
-import java.util.stream.Collectors;
 
 public class HomeCommand extends Command {
+    private static File dataFile = null;
     private final Map<UUID, List<Home>> homes = new HashMap<>();
 
-    @Override
-    public void preinit() throws Exception {
-        PlayerEvent.PLAYER_JOIN.register(player -> {
-            if (homes.containsKey(player.getUUID())) return; // Still in cache
-
-            getLocalDb().selectBuilder("homes")
-                    .select("*")
-                    .where(QueryCondition.equals("owner", player.getUUID()))
-                    .executeAsync()
-                    .thenAccept(res -> {
-                        homes.put(player.getUUID(), getLocalDb().select("homes", "*", QueryCondition.equals("owner", player.getUUID())).stream()
-                                .map(row -> new Home(row.getString("name"), row.getDouble("x"), row.getDouble("y"), row.getDouble("z"),
-                                        row.getFloat("pitch"), row.getFloat("yaw"), row.get("dimension", ResourceLocation.class))).collect(Collectors.toList()));
-                    });
-        });
-
-        PlayerEvent.PLAYER_QUIT.register(player -> {
-            MinecraftServer server0 = player.getServer();
-            UUID id = player.getUUID();
-            scheduleTask(() -> {
-                if (Objects.requireNonNull(server0).getPlayerList().getPlayer(id) == null) homes.remove(id);
-            }, 600); // Clear cache if player does not relog within 30 seconds
-        });
-    }
-
-    public void init(boolean serverOnly, MinecraftServer server) throws IOException {
-        Path dataFile = MoreCommands.getRelativePath().resolve("homes.json");
+    public void init(boolean serverOnly, MinecraftServer server) {
+        if (MoreCommandsArch.getConfigDirectory().resolve("homes.json").toFile().exists())
+            MoreCommands.tryMove("config/MoreCommands/homes.json", MoreCommands.getRelativePath().resolve("homes.json").toString());
+        dataFile = MoreCommands.getRelativePath().resolve("homes.json").toFile();
         Map<String, Map<String, Map<String, Object>>> data = null;
         try {
             data = MoreCommands.readJson(dataFile);
@@ -67,43 +39,12 @@ public class HomeCommand extends Command {
             log.catching(e);
         } catch (NullPointerException ignored) {}
         homes.clear();
-
-        TablePreset.create("homes")
-                .putColumn("owner", ColumnType.CHAR.createStructure()
-                        .configure(f -> f.apply(36))
-                        .setPrimary(true)
-                        .setNullAllowed(false))
-                .putColumn("name", ColumnType.VARCHAR.createStructure()
-                        .configure(f -> f.apply(255))
-                        .setNullAllowed(false))
-                .putColumn("x", ColumnType.DOUBLE.createStructure()
-                        .configure(f -> f.apply(null, null)))
-                .putColumn("y", ColumnType.DOUBLE.createStructure()
-                        .configure(f -> f.apply(null, null)))
-                .putColumn("z", ColumnType.DOUBLE.createStructure()
-                        .configure(f -> f.apply(null, null)))
-                .putColumn("pitch", ColumnType.FLOAT.createStructure()
-                        .configure(f -> f.apply(null, null)))
-                .putColumn("yaw", ColumnType.FLOAT.createStructure()
-                        .configure(f -> f.apply(null, null)))
-                .putColumn("dimension", ColumnType.TEXT.createStructure())
-                .addIndex(TableIndex.index("name", TableIndex.Type.INDEX))
-                .create(getLocalDb());
-
-        Files.delete(dataFile);
-        if (data == null) return;
-
-        InsertBuilder insert = getLocalDb().insertBuilder("homes", "owner", "name", "x", "y", "z", "pitch", "yaw", "dimension");
-        data.forEach((key, value) -> {
-            UUID owner = UUID.fromString(key);
-            List<Home> homes = new ArrayList<>();
-
-            for (Map.Entry<String, Map<String, Object>> entry : value.entrySet()) {
-                homes.add(Home.fromMap(entry));
-                insert.insert(owner, entry.getKey(), entry.getValue().get("x"), entry.getValue().get("y"), entry.getValue().get("z"), entry.getValue().get("pitch"), entry.getValue().get("yaw"), entry.getValue().get("dimension"));
-            }
-            this.homes.put(owner, homes);
-        });
+        if (data != null)
+            data.forEach((key, value) -> {
+                List<Home> homes = new ArrayList<>();
+                value.entrySet().forEach(entry0 -> homes.add(Home.fromMap(entry0)));
+                this.homes.put(UUID.fromString(key), homes);
+            });
     }
 
     @Override
@@ -131,7 +72,7 @@ public class HomeCommand extends Command {
                             else {
                                 getHomes(p).remove(home);
                                 if (getHomes(p).isEmpty()) homes.remove(p.getUUID());
-                                getLocalDb().delete("homes", QueryCondition.equals("owner", p.getUUID()).and(QueryCondition.equals("name", home.name)));
+                                saveData();
                                 sendMsg(ctx, "Your home " + SF + home.name + DF + " was removed.");
                                 return 1;
                             }
@@ -154,12 +95,9 @@ public class HomeCommand extends Command {
         else if (getHomes(p).size() >= max && !bypass) sendError(ctx, "You cannot set more than " + max + " homes.");
         else {
             if (!homes.containsKey(p.getUUID())) homes.put(p.getUUID(), new ArrayList<>());
-
-            Home home = new Home(name, p.position().x, p.position().y, p.position().z, ((MixinEntityAccessor) p).getXRot_(), ((MixinEntityAccessor) p).getYRot_(), p.getCommandSenderWorld().dimension().location());
-            getHomes(p).add(home);
-            getLocalDb().insert("homes", new String[] {"owner", "name", "x", "y", "z", "pitch", "yaw", "dimension"},
-                    new Object[] {p.getUUID(), home.name, home.x, home.y, home.z, home.pitch, home.yaw, home.dimension});
-
+            getHomes(p).add(new Home(name, p.position().x, p.position().y, p.position().z, ((MixinEntityAccessor) p).getXRot_(),
+                    ((MixinEntityAccessor) p).getYRot_(), p.getLevel().dimension().location()));
+            saveData();
             sendMsg(ctx, "A home by the name of " + SF + name + DF + " has been set.");
         }
         return homes.get(p.getUUID()).size();
@@ -178,14 +116,15 @@ public class HomeCommand extends Command {
     }
 
     private int sendHomes(Player player) {
-        sendMsg(player, getHomes(player).isEmpty() ? ChatFormatting.RED + "You do not have any homes set yet, set one with /sethome [name]." : "You have set the following homes: " +
-                joinNicely(getHomes(player).stream().collect(Collector.of(ArrayList::new, (l, home) -> l.add(home.name), BinaryOperator.maxBy(Comparator.comparingInt(List::size))))) + ".");
+        sendMsg(player, getHomes(player).isEmpty() ? ChatFormatting.RED + "You do not have any homes set yet, set one with /sethome [name]." :
+                "You have set the following homes: " + joinNicely(getHomes(player).stream()
+                        .collect(Collector.of(ArrayList::new, (l, home) -> l.add(home.name), BinaryOperator.maxBy(Comparator.comparingInt(List::size))))) + ".");
         return getHomes(player).size();
     }
 
     private int tpHome(Player player, Home home) {
         MoreCommands.teleport(player, Objects.requireNonNull(player.getServer()).getLevel(ResourceKey.create(Registry.DIMENSION_REGISTRY, home.dimension)), home.x, home.y, home.z, home.yaw, home.pitch);
-        ResourceKey<Level> registryKey = player.getCommandSenderWorld().dimension();
+        ResourceKey<Level> registryKey = player.getLevel().dimension();
         if (Level.NETHER.equals(registryKey)) return 9;
         else if (Level.OVERWORLD.equals(registryKey)) return 10;
         else if (Level.END.equals(registryKey)) return 11;
@@ -203,7 +142,23 @@ public class HomeCommand extends Command {
         return homes.getOrDefault(player.getUUID(), Collections.emptyList());
     }
 
+    private void saveData() {
+        Map<String, Map<String, Map<String, Object>>> data = new HashMap<>();
+        for (Map.Entry<UUID, List<Home>> entry : homes.entrySet()) {
+            Map<String, Map<String, Object>> homes = new HashMap<>();
+            for (Home home : entry.getValue())
+                homes.put(home.name, home.toMap());
+            data.put(entry.getKey().toString(), homes);
+        }
+        try {
+            MoreCommands.saveJson(dataFile, data);
+        } catch (IOException e) {
+            log.catching(e);
+        }
+    }
+
     private static class Home {
+
         private final String name;
         private final double x, y, z;
         private final float pitch, yaw;
@@ -219,9 +174,22 @@ public class HomeCommand extends Command {
             this.dimension = dimension;
         }
 
+        private Map<String, Object> toMap() {
+            Map<String, Object> data = new HashMap<>();
+            data.put("x", x);
+            data.put("y", y);
+            data.put("z", z);
+            data.put("pitch", pitch);
+            data.put("yaw", yaw);
+            data.put("dimension", dimension.toString());
+            return data;
+        }
+
         private static Home fromMap(Map.Entry<String, Map<String, Object>> data) {
             Map<String, Object> v = data.getValue();
-            return new Home(data.getKey(), (Double) v.get("x"), (Double) v.get("y"), (Double) v.get("z"), ((Double) v.get("pitch")).floatValue(), ((Double) v.get("yaw")).floatValue(), new ResourceLocation((String) v.get("dimension")));
+            return new Home(data.getKey(), (Double) v.get("x"), (Double) v.get("y"), (Double) v.get("z"),
+                    ((Double) v.get("pitch")).floatValue(), ((Double) v.get("yaw")).floatValue(),
+                    new ResourceLocation((String) v.get("dimension")));
         }
     }
 }
