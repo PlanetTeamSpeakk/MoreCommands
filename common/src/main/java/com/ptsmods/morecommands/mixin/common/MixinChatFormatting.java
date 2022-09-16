1d800d64f119e47ef0bc14f0257bb22d325607fd
PlanetTeamSpeak <PlanetTeamSpeakk@users.noreply.github.com>
2022-09-16 20:20:24 +0200

    Fix Rainbow formatting not working The mixin just wasn't being registered

--- common/src/main/java/com/ptsmods/morecommands/mixin/common/MixinChatFormatting.java
+++ common/src/main/java/com/ptsmods/morecommands/mixin/common/MixinChatFormatting.java
@@ -5,5 +5,5 @@ import org.spongepowered.asm.mixin.Mixin;
 
 // Empty mixin, only used so we get the pre-mixin ClassNode in the plugin.
 @Mixin(ChatFormatting.class)
-public class MixinFormatting {
+public class MixinChatFormatting {
 }
