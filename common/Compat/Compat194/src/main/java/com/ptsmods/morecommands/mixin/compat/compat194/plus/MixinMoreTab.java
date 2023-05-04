e3a48d79741649985b7e479b7e5b6bc166a27226
PlanetTeamSpeak <PlanetTeamSpeakk@users.noreply.github.com>
2023-05-05 00:46:08 +0200

    Fix MixinMoreTab not loading

--- common/Compat/Compat194/src/main/java/com/ptsmods/morecommands/mixin/compat/compat194/plus/MixinMoreTab.java
+++ common/Compat/Compat194/src/main/java/com/ptsmods/morecommands/mixin/compat/compat194/plus/MixinMoreTab.java
@@ -6,9 +6,7 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.layouts.GridLayout;
 import net.minecraft.client.gui.screens.worldselection.CreateWorldScreen;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -16,11 +14,10 @@ import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(targets = "net/minecraft/client/gui/screens/worldselection/CreateWorldScreen$MoreTab")
 public abstract class MixinMoreTab {
-    @Shadow @Final CreateWorldScreen this$0;
-
     @Inject(at = @At("RETURN"), method = "<init>", locals = LocalCapture.CAPTURE_FAILHARD)
-    private void init(CallbackInfo cbi, GridLayout.RowHelper rowHelper) {
-        rowHelper.addChild(Button.builder(LiteralTextBuilder.literal("Initialisation Commands"), btn -> Minecraft.getInstance().setScreen(IMoreCommandsClient.get().newWorldInitScreen(this$0)))
+    private void init(CreateWorldScreen parent, CallbackInfo cbi, GridLayout.RowHelper rowHelper) {
+        rowHelper.addChild(Button.builder(LiteralTextBuilder.literal("Initialisation Commands"), btn ->
+                        Minecraft.getInstance().setScreen(IMoreCommandsClient.get().newWorldInitScreen(parent)))
                 .width(210)
                 .build());
     }
