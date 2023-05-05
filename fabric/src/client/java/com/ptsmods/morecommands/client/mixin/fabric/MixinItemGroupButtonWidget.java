c7cc82f5668e97211970956f7902da7a600ad66d
PlanetTeamSpeak <PlanetTeamSpeakk@users.noreply.github.com>
2023-05-05 13:28:59 +0200

    Fix MixinItemGroupButtonWidget

--- fabric/src/client/java/com/ptsmods/morecommands/client/mixin/fabric/MixinItemGroupButtonWidget.java
+++ fabric/src/client/java/com/ptsmods/morecommands/client/mixin/fabric/MixinItemGroupButtonWidget.java
@@ -2,6 +2,7 @@ package com.ptsmods.morecommands.client.mixin.fabric;
 
 import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.PoseStack;
+import com.ptsmods.morecommands.api.ReflectionHelper;
 import com.ptsmods.morecommands.clientoption.ClientOptions;
 import net.fabricmc.fabric.impl.client.itemgroup.FabricCreativeGuiComponents;
 import net.minecraft.resources.ResourceLocation;
@@ -18,11 +19,13 @@ public class MixinItemGroupButtonWidget {
     private static final @Unique ResourceLocation BUTTON_TEX = new ResourceLocation("morecommands:textures/gui/creative_buttons.png");
 
     @Redirect(at = @At(value = "INVOKE", target = "Lnet/fabricmc/fabric/impl/client/itemgroup/FabricCreativeGuiComponents$ItemGroupButtonWidget;blit(Lcom/mojang/blaze3d/vertex/PoseStack;IIIIII)V"), method = "render")
-    private void render_drawTexture_height(FabricCreativeGuiComponents.ItemGroupButtonWidget itemGroupButtonWidget, PoseStack matrices, int x, int y, int u, int v, int width, int height) {
+    private void render_drawTexture_height(PoseStack matrices, int x, int y, int u, int v, int width, int height) {
+        FabricCreativeGuiComponents.ItemGroupButtonWidget thiz = ReflectionHelper.cast(this);
+
         if (ClientOptions.Rendering.bigTabPager.getValue()) {
             RenderSystem.setShaderTexture(0, BUTTON_TEX);
-            itemGroupButtonWidget.blit(matrices, x, y, (itemGroupButtonWidget.active && itemGroupButtonWidget.isHoveredOrFocused() ? 44 : 0) +
-                    (type == FabricCreativeGuiComponents.Type.NEXT ? 22 : 0), itemGroupButtonWidget.active ? 0 : 22, 22, 22);
-        } else itemGroupButtonWidget.blit(matrices, x, y, u, v, width, height);
+            FabricCreativeGuiComponents.ItemGroupButtonWidget.blit(matrices, x, y, (thiz.active && thiz.isHoveredOrFocused() ? 44 : 0) +
+                    (type == FabricCreativeGuiComponents.Type.NEXT ? 22 : 0), thiz.active ? 0 : 22, 22, 22);
+        } else FabricCreativeGuiComponents.ItemGroupButtonWidget.blit(matrices, x, y, u, v, width, height);
     }
 }
