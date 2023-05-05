8d0d888b3ef94283e40e7bfc9379e1fe52a5a400
PlanetTeamSpeak <PlanetTeamSpeakk@users.noreply.github.com>
2023-05-05 13:58:00 +0200

    Fix opening inventory in creative crashing the game on 1.19.4

--- common/Compat/Compat194/src/main/java/com/ptsmods/morecommands/compat/client/ClientCompat194.java
+++ common/Compat/Compat194/src/main/java/com/ptsmods/morecommands/compat/client/ClientCompat194.java
@@ -1,10 +1,32 @@
 package com.ptsmods.morecommands.compat.client;
 
+import com.ptsmods.morecommands.api.util.compat.Compat;
+import dev.architectury.registry.CreativeTabRegistry;
 import net.minecraft.client.gui.components.EditBox;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 
 public class ClientCompat194 extends ClientCompat193 {
+    private static CreativeTabRegistry.TabSupplier unobtainableItemsTab;
+
     @Override
     public void setFocused(EditBox editBox, boolean focused) {
         editBox.setFocused(focused);
+    }
+
+    @Override
+    public void registerUnobtainableItemsTab() {
+        if (unobtainableItemsTab != null) return;
+
+        unobtainableItemsTab = CreativeTabRegistry.create(new ResourceLocation("morecommands:unobtainable_items"), builder -> builder
+                .icon(() -> new ItemStack(Compat.get().<Item>getBuiltInRegistry("item")
+                        .get(new ResourceLocation("morecommands:locked_chest"))))
+                .displayItems(((flags, output) -> {
+                    for (Item item : Compat.get().<Item>getBuiltInRegistry("item"))
+                        if (item != Items.AIR && CreativeModeTabs.allTabs().stream().noneMatch(tab -> tab.contains(new ItemStack(item)))) output.accept(item);
+                })));
     }
 }
