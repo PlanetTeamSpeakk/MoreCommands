c905ff83f83702be6fa31c41317227f86b297b16
PlanetTeamSpeak <PlanetTeamSpeakk@users.noreply.github.com>
2023-05-07 18:46:35 +0200

    Actually fix it

--- common/src/main/java/com/ptsmods/morecommands/commands/unelevated/CyclePaintingCommand.java
+++ common/src/main/java/com/ptsmods/morecommands/commands/unelevated/CyclePaintingCommand.java
@@ -39,7 +39,7 @@ public class CyclePaintingCommand extends Command {
         ResourceLocation oldArt = Compat.get().getPaintingVariant(painting);
         Compat.get().setPaintingVariant(painting, variant == null ? Compat.get().nextPaintingVariant(oldArt) : variant);
 
-        if (Version.getCurrent().isOlderThan(Version.V1_19)) return 1; // Below code only necessary on old versions
+        if (Version.getCurrent().isNewerThanOrEqual(Version.V1_19)) return 1; // Below code only necessary on old versions
         BlockPos pos = Compat.get().blockPosition(painting);
         Entity painting0 = MoreCommands.cloneEntity(painting, false);
         painting.kill();
