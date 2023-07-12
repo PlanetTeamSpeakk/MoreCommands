b73f6981ae27be9fea9b0987293bb6c45f336fd4
PlanetTeamSpeak <PlanetTeamSpeakk@users.noreply.github.com>
2023-07-12 16:55:18 +0200

    Clean up ScreenshotCommand

--- common/src/client/java/com/ptsmods/morecommands/client/commands/ScreenshotCommand.java
+++ common/src/client/java/com/ptsmods/morecommands/client/commands/ScreenshotCommand.java
@@ -10,13 +10,14 @@ import com.ptsmods.morecommands.api.callbacks.RenderTickEvent;
 import com.ptsmods.morecommands.client.miscellaneous.ClientCommand;
 import com.ptsmods.morecommands.client.mixin.accessor.MixinWindowAccessor;
 import dev.architectury.event.events.client.ClientTickEvent;
-import org.lwjgl.opengl.GL11;
-
-import javax.imageio.ImageIO;
+import lombok.Data;
 import net.minecraft.ChatFormatting;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.multiplayer.ClientSuggestionProvider;
 import net.minecraft.network.chat.ClickEvent;
+import org.lwjgl.opengl.GL11;
+
+import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -24,12 +25,10 @@ import java.nio.ByteBuffer;
 import java.nio.file.Paths;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
 
 public class ScreenshotCommand extends ClientCommand {
-    private Map<String, Object> queue = null;
-    private Map<String, Object> task = null;
+    private ScreenshotTask queue = null;
+    private ScreenshotTask task = null;
 
     public void preinit() {
         ClientTickEvent.CLIENT_POST.register(client -> {
@@ -41,37 +40,40 @@ public class ScreenshotCommand extends ClientCommand {
 
         RenderTickEvent.POST.register(tick -> {
             if (task != null) {
-                long startTake = System.currentTimeMillis();
-                task.put("tries", (int) task.get("tries") + 1);
-                if ((int) task.get("tries") > 2) {
+                if (task.tries++ > 2) {
                     task = null;
                     return;
                 }
-                int width = (int) task.get("width");
-                int height = (int) task.get("height");
+
+                long startTake = System.currentTimeMillis();
                 GL11.glPixelStorei(GL11.GL_PACK_ALIGNMENT, 1);
                 GL11.glPixelStorei(GL11.GL_UNPACK_ALIGNMENT, 1);
-                ByteBuffer buf = ByteBuffer.allocateDirect(width * height * 3);
+                ByteBuffer buf = ByteBuffer.allocateDirect(task.getWidth() * task.getHeight() * 3);
                 RenderTarget fb = Minecraft.getInstance().getMainRenderTarget();
                 GL11.glBindTexture(GL11.GL_TEXTURE_2D, fb.getColorTextureId());
                 GL11.glGetTexImage(GL11.GL_TEXTURE_2D, 0, GL11.GL_RGB, GL11.GL_UNSIGNED_BYTE, buf);
-                ReflectionHelper.<MixinWindowAccessor>cast(Minecraft.getInstance().getWindow()).callOnFramebufferResize(Minecraft.getInstance().getWindow().getWindow(), (int) task.get("ogWidth"), (int) task.get("ogHeight"));
-                Map<String, Object> task = this.task;
+                ReflectionHelper.<MixinWindowAccessor>cast(Minecraft.getInstance().getWindow()).callOnFramebufferResize(Minecraft.getInstance().getWindow().getWindow(),
+                        task.getOriginalWidth(), task.getOriginalHeight());
+
+                ScreenshotTask task = this.task;
                 this.task = null;
                 long takeTime = System.currentTimeMillis() - startTake;
+
                 MoreCommands.execute(() -> {
                     sendMsg("Saving the screenshot, this may take a while depending on its dimensions...");
                     long start = System.currentTimeMillis();
                     try {
-                        BufferedImage img = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
-                        for (int x = 0; x < width; x++)
-                            for (int y = height - 1; y >= 0; y--) { // Image is flipped because this game starts drawing at the bottom left instead of the top left for some reason.
-                                int i = (x + width * y) * 3;
-                                img.setRGB(x, height - y - 1, shiftRGB(buf.get(i) & 0xFF, buf.get(i + 1) & 0xFF, buf.get(i + 2) & 0xFF));
+                        BufferedImage img = new BufferedImage(task.getWidth(), task.getHeight(), BufferedImage.TYPE_INT_RGB);
+                        for (int x = 0; x < task.getWidth(); x++)
+                            for (int y = task.getHeight() - 1; y >= 0; y--) { // Image is flipped because OpenGL starts drawing at the bottom left instead of the top left for some reason.
+                                int i = (x + task.getWidth() * y) * 3;
+                                img.setRGB(x, task.getHeight() - y - 1, shiftRGB(buf.get(i) & 0xFF, buf.get(i + 1) & 0xFF, buf.get(i + 2) & 0xFF));
                             }
-                        String fileName = new SimpleDateFormat("yyyy-MM-dd_HH.mm.ss_" + task.get("width") + "'x'" + task.get("height") + ".'png'").format(new Date());
+
+                        String fileName = new SimpleDateFormat("yyyy-MM-dd_HH.mm.ss_" + task.getWidth() + "'x'" + task.getHeight() + ".'png'").format(new Date());
                         File out = Paths.get(Minecraft.getInstance().gameDirectory.getAbsolutePath(), "screenshots", fileName).toFile();
                         if (!out.getParentFile().exists()) out.getParentFile().mkdirs();
+
                         ImageIO.write(img, "png", out);
                         long saveTime = System.currentTimeMillis() - start;
                         sendMsg(literalText("Saved screenshot as ", DS)
@@ -79,7 +81,8 @@ public class ScreenshotCommand extends ClientCommand {
                                         .withStyle(SS
                                                 .applyFormat(ChatFormatting.UNDERLINE)
                                                 .withClickEvent(new ClickEvent(ClickEvent.Action.OPEN_FILE, out.getCanonicalPath()))))
-                                .append(literalText(", took " + (saveTime + takeTime) / 1000 + " seconds (" + takeTime / 1000 + " seconds to take the screenshot and " + saveTime / 1000 + " seconds to save it).", DS)));
+                                .append(literalText(", took " + (saveTime + takeTime) / 1000 + " seconds (" + takeTime / 1000 +
+                                        " seconds to take the screenshot and " + saveTime / 1000 + " seconds to save it).", DS)));
                     } catch (IOException e) {
                         log.catching(e);
                         sendMsg(ChatFormatting.RED + "An unknown error occurred while saving the image: " + SF + e.getMessage() + DF + ".");
@@ -118,16 +121,11 @@ public class ScreenshotCommand extends ClientCommand {
         height = height == -1 ? Minecraft.getInstance().getWindow().getScreenHeight() : height;
         int ogWidth = Minecraft.getInstance().getWindow().getScreenWidth();
         int ogHeight = Minecraft.getInstance().getWindow().getScreenHeight();
-        if (width * height * 3 < 0) sendMsg(ChatFormatting.RED + "The given dimensions are too big. The product of the width and height may at most be " + Integer.MAX_VALUE / 3 + " (product was " + (long) width * (long) height + ").");
+        if (width * height * 3 < 0) sendMsg(ChatFormatting.RED + "The given dimensions are too big. " +
+                "The product of the width and height may at most be " + Integer.MAX_VALUE / 3 +
+                " (product was " + (long) width * (long) height + ").");
         else {
-            Map<String, Object> task = new HashMap<>();
-            task.put("context", ctx);
-            task.put("ogWidth", ogWidth);
-            task.put("ogHeight", ogHeight);
-            task.put("width", width);
-            task.put("height", height);
-            task.put("tries", 0);
-            queue = task;
+            queue = new ScreenshotTask(ctx, ogWidth, ogHeight, width, height);
             ReflectionHelper.<MixinWindowAccessor>cast(Minecraft.getInstance().getWindow()).callOnFramebufferResize(Minecraft.getInstance().getWindow().getWindow(), width, height);
         }
         return 1;
@@ -137,5 +135,13 @@ public class ScreenshotCommand extends ClientCommand {
         return (r & 0xFF) << 16 | // Red
                 (g & 0xFF) << 8 | // Green
                 (b & 0xFF); // Blue
+    }
+
+    @Data
+    private static class ScreenshotTask {
+        public final CommandContext<ClientSuggestionProvider> ctx;
+        public final int originalWidth, originalHeight;
+        public final int width, height;
+        public int tries = 0;
     }
 }
